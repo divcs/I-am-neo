19300cd2d2c256b5ea047847a7ae80030ab4beab
Divya <singhdivya10aug2002@gmail.com>
2024-11-06 19:33:02 +0530

    Vehicle

--- CodeBlock/C++/U3 L14 Vehicle/main.cpp
+++ CodeBlock/C++/U3 L14 Vehicle/main.cpp
@@ -12,15 +12,27 @@ public:
     Vehicle(string r, string m, string model, int y):regNo(r),makeV(m),modelV(model),yearM(y){}
 
     void displayDetails(){
-    cout<<"Reg No: "<<regNo<<endl;
-    cout<<"Reg No: "<<regNo<<endl;
-    cout<<"Reg No: "<<regNo<<endl;
-    cout<<"Reg No: "<<regNo<<endl;
+    cout<<"Registration Number: "<<regNo<<endl;
+    cout<<"Make: "<<makeV<<endl;
+    cout<<"Model: "<<modelV<<endl;
+    cout<<"Year of Manufacture: "<<yearM<<endl;
     }
 };
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string r, mv, mov;
+    int y;
+    cout<<"Registration Number: ";
+    cin>>r;
+    cout<<"Make: ";
+    cin>>mv;
+    cout<<"Model: ";
+    cin>>mov;
+    cout<<"Year of Manufacture: ";
+    cin>>y;
+
+    Vehicle v(r,mv,mov,y);
+    v.displayDetails();
     return 0;
 }
