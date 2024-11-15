4d3c3c8184269b8893e55179ed6a7452c66643e4
Divya <singhdivya10aug2002@gmail.com>
2024-11-15 20:40:46 +0530

    string adding

---
+++ cpp_practice_and_learning/bca_learnings/add_strings.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     string a,b,sum;
+
+    cout<<"Enter two strings to add:";
+    cin>>a;
+    cin>>b;
+
+    sum=a+b;
+
+    cout<<"Sum is : "<<sum;
+
+    return 0;
+}
